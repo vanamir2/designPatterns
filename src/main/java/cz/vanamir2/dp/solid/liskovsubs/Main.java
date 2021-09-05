b7e2c1a8b8411853e03f74747caa6b53896233a8
vanamir2 <miravana10@seznam.cz>
2021-09-05 04:59:26 +0200

    L from SOLID Liskov substitution principle

---
+++ src/main/java/cz/vanamir2/dp/solid/liskovsubs/Main.java
@@ -0,0 +1,12 @@
+package cz.vanamir2.dp.solid.liskovsubs;
+
+
+/**
+ * You should be able to substitute Subclass (derived class) for a Base class
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
---
+++ src/main/java/cz/vanamir2/dp/solid/liskovsubs/RectangleFactory.java
@@ -0,0 +1,14 @@
+package cz.vanamir2.dp.solid.liskovsubs;
+
+// if you want an explicit Rectangle inicialization - you use factory, not inherited class
+public class RectangleFactory {
+
+    public static Rectangle newRectangle(int width, int height) {
+        return new Rectangle(width, height);
+    }
+
+    public static Rectangle newSquare(int side) {
+        return new Rectangle(side, side);
+    }
+
+}
